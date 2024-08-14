02cdde234e23da8ca268b30a48700115ae6cb7da
adogbeji <baka_1993@yahoo.co.uk>
2024-08-14 13:50:10 +0100

    Start adding name input field & wrap register screen with Padding

--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -5,18 +5,32 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Register', style: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),),
-          ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              ),
+        
+              // NAME INPUT FIELD
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Enter Name...',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
