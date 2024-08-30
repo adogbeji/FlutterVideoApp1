7cb050aff67c65e7a7c31f2769f4acd00b627c5c
adogbeji <baka_1993@yahoo.co.uk>
2024-08-30 13:56:47 +0100

    Start adding code to AuthController class

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,1 +1,8 @@
-class AuthController {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+
+  static AuthController instanceAuth = Get.find();  // Stores instance of AuthController class
+  late Rx<User?> _currentUser;  // Stores instance of user
+}
