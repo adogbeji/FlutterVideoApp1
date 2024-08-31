a9b754728258f9f146a1a0a8df4f2269026481a5
adogbeji <baka_1993@yahoo.co.uk>
2024-08-31 14:15:05 +0100

    Start creating function to pick image from phone gallery in AuthController class

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AuthController extends GetxController {
 
   static AuthController instanceAuth = Get.find();  // Stores instance of AuthController class
   late Rx<User?> _currentUser;  // Stores instance of user
+
+  late Rx<File?> _pickedFile;  // Stores image user picked from phone gallery
+  File? get profileImage => _pickedFile.value;
+
+
+  // Picks image from phone gallery
+  Future<void> chooseImageFromGallery() async {
+
+    final pickedImageFile = await ImagePicker().pickImage(source: ImageSource.gallery);  // Stores picked image
+  }
 }
