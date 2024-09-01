d478200457c8c8354be37c84d49bb95309c5b22d
adogbeji <baka_1993@yahoo.co.uk>
2024-09-01 17:51:10 +0100

    Add extra code to select image from gallery & add function to capture image with phone camera

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -17,5 +17,29 @@ class AuthController extends GetxController {
   Future<void> chooseImageFromGallery() async {
 
     final pickedImageFile = await ImagePicker().pickImage(source: ImageSource.gallery);  // Stores picked image
+
+    if (pickedImageFile != null) {
+      Get.snackbar(
+        'Profile Image', 
+        'Profile picture chosen!'
+      );
+    }
+
+    _pickedFile = Rx<File?>(File(pickedImageFile!.path));  // Converts picked image to type Rx<File>
+  }
+
+  // Captures image with phone camera
+  Future<void> captureImageWithCamera() async {
+
+    final pickedImageFile = await ImagePicker().pickImage(source: ImageSource.camera);  // Stores captured image
+
+    if (pickedImageFile != null) {
+      Get.snackbar(
+        'Profile Image', 
+        'Profile picture captured!'
+      );
+    }
+
+    _pickedFile = Rx<File?>(File(pickedImageFile!.path));  // Converts picked image to type Rx<File>
   }
 }
