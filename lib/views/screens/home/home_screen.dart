81718aa7d6639fcb6160f07ea90388193a8c538d
adogbeji <baka_1993@yahoo.co.uk>
2024-09-19 14:04:02 +0100

    Add more items to bottom navigation bar in home screen

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -12,14 +12,24 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.amber,
+        selectedItemColor: Colors.red,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home, size: 16,),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.search, size: 16,),
             label: 'Discover',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add, size: 16,),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox_sharp),
+            label: 'Following',
           ),
         ],
       ),
