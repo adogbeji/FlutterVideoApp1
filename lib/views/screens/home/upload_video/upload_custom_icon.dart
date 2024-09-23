05c0f007d5cbadbdb7755fee2db25e5e37a1fa45
adogbeji <baka_1993@yahoo.co.uk>
2024-09-23 14:54:00 +0100

    Add more styling for custom upload icon

--- lib/views/screens/home/upload_video/upload_custom_icon.dart
+++ lib/views/screens/home/upload_video/upload_custom_icon.dart
@@ -5,15 +5,23 @@ class UploadCustomIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       width: 46,
       height: 32,
       child: Stack(
         children: [
           Center(
-            child: Icon(
-              Icons.add,
-              size: 24,
+            child: Container(
+              width: 40,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Icon(
+                Icons.add,
+                size: 24,
+              ),
             ),
           ),
         ],
