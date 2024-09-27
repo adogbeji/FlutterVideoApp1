2b34cdaad7efed8fdce8edd7d1cd701ca7128f70
adogbeji <baka_1993@yahoo.co.uk>
2024-09-27 13:51:26 +0100

    Start building upload video screen

--- lib/views/screens/home/upload_video/upload_video_screen.dart
+++ lib/views/screens/home/upload_video/upload_video_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:video_app_1/views/screens/home/upload_video/upload_form.dart';
 
 class UploadVideoScreen extends StatefulWidget {
   const UploadVideoScreen({super.key});
@@ -8,6 +15,93 @@ class UploadVideoScreen extends StatefulWidget {
 }
 
 class _UploadVideoScreenState extends State<UploadVideoScreen> {
+  getVideoFile(ImageSource source) async {
+    final videoFile = await ImagePicker().pickVideo(source: source);  // Stores video file
+
+    if (videoFile != null) {
+      // Get.to(
+      //   UploadForm(
+      //     videoFile:
+      //         File(videoFile.path), // File which is available at this path
+      //     videoPath: videoFile.path,
+      //   ),
+      // );
+    }
+  }
+
+  displayDialogBox() {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        children: [
+          SimpleDialogOption(
+            onPressed: () {
+              getVideoFile(ImageSource.gallery);
+            },
+            child: const Row(
+              children: [
+                Icon(Icons.image),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Get Video From Gallery',
+                      maxLines: 3,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {
+              getVideoFile(ImageSource.camera);
+            },
+            child: const Row(
+              children: [
+                Icon(Icons.camera_alt),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Capture Video With Camera',
+                      maxLines: 3,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {
+              Get.back(); // Closes dialog box
+            },
+            child: const Row(
+              children: [
+                Icon(Icons.cancel),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
