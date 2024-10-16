00e7d54d4038ab0f39bd4324e0b0ddb120034a94
adogbeji <baka_1993@yahoo.co.uk>
2024-10-16 12:14:42 +0100

    Start adding code to upload compressed video in UploadController

--- lib/controllers/upload_controller.dart
+++ lib/controllers/upload_controller.dart
@@ -1,16 +1,22 @@
 import 'package:get/get.dart';
 import 'package:video_compress/video_compress.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class UploadController extends GetxController {
-
+  
+  // Compresses Video
   compressVideoFile(String videoFilePath) async {
     final compressedVideoFilePath = await VideoCompress.compressVideo(videoFilePath, quality: VideoQuality.LowQuality);  // Stores compressed video file path
   
     return compressedVideoFilePath;  // Returns compressed video
   }
+  
+  // Uploads Compressed Video
+  uploadCompressedVideo(String videoID, String videoFile) async {
+    UploadTask videoUploadTask = FirebaseStorage.instance.ref().child('videos');
+  }
 
-  uploadCompressedVideo(String videoID, String videoFile) {}
-
+  // Gets Video Thumbnail
   getThumbnailImage(String videoFilePath) async {
     final thumbnailImage = await VideoCompress.getFileThumbnail(videoFilePath);  // Stores thumbnail image
 
